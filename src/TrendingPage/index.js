eb058133a4edfc453615d14f79d47a7b5ef8b6b5
Jeff Boulay <BoulayJ@DNB.com>
2017-11-23 14:41:42 -0600

    added state to trending section

--- src/TrendingPage/index.js
+++ src/TrendingPage/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+export default class Trending extends React.Component {
 
-const Trending = () => (
-    <div>
-        <h1>Trending Hot Sauces</h1>
-        <ol>
-            <li>Brain Melter</li>
-            <li>Atomic Destruction</li>
-            <li>Burnanator</li>
-        </ol>
-        <Link to="/">Back</Link>
-    </div>
-);
+    constructor (props){
+        super(props);
+        this.state = {
+            sauces:[]
+        }
+    }
 
-export default Trending;
+    componentDidMount() {
+        const sauces = [
+            {
+                "id": 1,
+                "name": "Awesome Sauce",
+                "rating": 5
+            }
+        ]
+        this.setState({ sauces })
+    }
+    render() {
+        return(
+            <div>
+                <h1>Trending Hot Sauces</h1>
+                <ol>
+                    {this.state.sauces.map(sauce =>
+                        <li key={sauce.id}>{sauce.name} <strong>{sauce.rating}</strong></li>
+                    )}
+                </ol>
+                <Link to='/'>Back</Link>
+        </div>
+        )
+    }
+};
